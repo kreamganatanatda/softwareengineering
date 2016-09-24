509a9e63dcbf8ba6e675ab354a165465bfc68312
kreamganatanatda <kimhyorin.khr@gmail.com>
2016-09-24 08:59:00 +0700

    Add1

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Гертруда on 09.09.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World **");
+    }
+}
