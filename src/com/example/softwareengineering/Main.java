17dee83138e12d21e07853f9ae2cc6b9dfd81782
kreamganatanatda <kimhyorin.khr@gmail.com>
2016-10-08 00:00:42 +0700

    Rewrite main

--- src/com/example/softwareengineering/Main.java
+++ src/com/example/softwareengineering/Main.java
@@ -1,8 +1,5 @@
 package com.example.softwareengineering;
 
-//Класс, предоставляющий простой список, аналогичный массиву,
-//за исключением того, что количество элементов не фиксировано
-import java.util.ArrayList;
 
 import static com.example.softwareengineering.Permission.*;
 
@@ -10,15 +7,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<User> UserList = new ArrayList<>();
-        UserList.add(new User(1, "jdoe", "sup3rpaZZ", "John Doe" ));
-        UserList.add(new User(2, "jrow", "Qweqrty12", "Jane Row" ));
 
-        ArrayList<Role> RoleList = new ArrayList<>();
-        RoleList.add(new Role(1, 1, READ.code(), "a"));
-        RoleList.add(new Role(2, 1, WRITE.code(), "a.b"));
-        RoleList.add(new Role(3, 2, EXECUTE.code(), "a.b.c"));
-        RoleList.add(new Role(4, 1, EXECUTE.code(), "a.bc"));
+        User User1 = new User(1, "jdoe", "sup3rpaZZ", "John Doe" );
+        User User2 = new User(2, "jrow", "Qweqrty12", "Jane Row" );
+
+        Role Role1 = new Role(1, 1, READ.code(), "a");
+        Role Role2 = new Role(2, 1, WRITE.code(), "a.b");
+        Role Role3 = new Role(3, 2, EXECUTE.code(), "a.b.c");
+        Role Role4 = new Role(4, 1, EXECUTE.code(), "a.bc");
 
     System.out.println(args[0]+" "+args[1]+" "+args[2]);
     System.exit(0);
