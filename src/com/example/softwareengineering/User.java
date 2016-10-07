b59556c3de73aa70563b8a38b0e3e4222ecd5e94
kreamganatanatda <kimhyorin.khr@gmail.com>
2016-10-08 00:04:28 +0700

    Rewrite permissions

--- src/com/example/softwareengineering/User.java
+++ src/com/example/softwareengineering/User.java
@@ -1,19 +1,10 @@
 package com.example.softwareengineering;
 
-//Класс больших чисел
-import java.math.BigInteger;
-//То, что помогает генерировать соль\хэш
-import java.security.SecureRandom;
-
-import static com.example.softwareengineering.Secure.MD5;
-import static com.example.softwareengineering.Secure.generateSalt;
-
 class User {
     public int Id;
     public String Login;
     public String Password;
     public String Name;
-    //public String Salt;
 
 
     public User(int id, String login, String password, String name) {
@@ -21,8 +12,6 @@ class User {
         Login = login;
         Name = name;
         Password = password;
-       // Salt = generateSalt();
-       // Password = MD5(MD5(password)+Salt);
     }
 
 }
