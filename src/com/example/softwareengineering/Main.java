a51a9a57c6a357b0c5d0eb1991b30abebb2e1297
kreamganatanatda <kimhyorin.khr@gmail.com>
2016-10-08 01:46:14 +0700

    Добавлен массив anArrayOfRoles вместо четырех переменных Role1 Role2 Role3 Role4 для облегчения дальнейшей работы

--- src/com/example/softwareengineering/Main.java
+++ src/com/example/softwareengineering/Main.java
@@ -8,19 +8,18 @@ public class Main {
     public static void main(String[] args) {
 
         User[] anArrayOfUsers = new User[10];
-
-        //Role[] anArrayOfRoles = new Role[4];
+        Role[] anArrayOfRoles = new Role[4];
 
         anArrayOfUsers[0]= new User(1, "jdoe", "sup3rpaZZ", "John Doe" );
         anArrayOfUsers[1]= new User(2, "jrow", "Qweqrty12", "Jane Row" );
 
-       /* Role[0] = new Role(1, 1, READ.code(), "a");
-        Role[1] = new Role(2, 1, WRITE.code(), "a.b");
-        Role[2] = new Role(3, 2, EXECUTE.code(), "a.b.c");
-        Role[3] = new Role(4, 1, EXECUTE.code(), "a.bc");*/
+        anArrayOfRoles[0] = new Role(1, 1, READ.code(), "a");
+        anArrayOfRoles[1] = new Role(2, 1, WRITE.code(), "a.b");
+        anArrayOfRoles[2] = new Role(3, 2, EXECUTE.code(), "a.b.c");
+        anArrayOfRoles[3] = new Role(4, 1, EXECUTE.code(), "a.bc");
 
     System.out.println(args[0]+" "+args[1]+" "+args[2]);
-        System.out.println(anArrayOfUsers[1].Id);
+        System.out.println(anArrayOfRoles[1].Resource);
     System.exit(0);
     }
 }
