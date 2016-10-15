324416aa7751732ee5a0e9ef9853ccf0760bafdd
doublerin <tsuki.666.nya@gmail.com>
2016-10-15 09:33:59 +0700

    Update class Parse.java

--- src/com/example/softwareengineering/Parse.java
+++ src/com/example/softwareengineering/Parse.java
@@ -3,6 +3,7 @@ package com.example.softwareengineering;
 import org.apache.commons.cli.*;
 
 
+
 public class Parse {
     private Options options = new Options();
     private CommandLine cmdline;
@@ -26,22 +27,24 @@ public class Parse {
             cmdline = cmdLinePosixParser.parse(options, arg);
 
             if (cmdline.hasOption("l")) {
-                String[] arguments = cmdline.getOptionValues("l");
-                System.out.println("Login: " + arguments[0]);
+                /*[] arguments = cmdline.getOptionValues("l");
+                System.out.println("Login: " + arguments[0]);*/
+
+                System.out.println(cmdline.getOptionValue("l"));
             }
 
             if (cmdline.hasOption("p")) {
                 String[] arguments = cmdline.getOptionValues("p");
-                System.out.println("Password:" + arguments[0]);
+                System.out.println("Password:" + arguments[1]);
             }
 
             if (cmdline.hasOption("re")) {
                 String[] arguments = cmdline.getOptionValues("re");
-                System.out.println("Resource:" + arguments[0]);
+                System.out.println("Resource:" + arguments[2]);
             }
             if (cmdline.hasOption("ro")) {
                 String[] arguments = cmdline.getOptionValues("ro");
-                System.out.println("Role:" + arguments[0]);
+                System.out.println("Role:" + arguments[3]);
             }
         } catch (ParseException e) {
             if ((cmdline.hasOption("h"))||(cmdline==null)) {
